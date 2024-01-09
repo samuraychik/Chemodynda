7c88c364e88af64f54dbef3cc4212bc4de512d5d
Tarakanoed <svitch1337@gmail.com>
2024-01-09 17:23:01 +0500

    Помним
    save

--- domain/scripts/inventory.gd
+++ domain/scripts/inventory.gd
@@ -20,6 +20,7 @@ func add_item(item: Item, count: int):
 	if not item in items.keys():
 		items[item] = 0
 	items[item] += count
+	save_character()
 
 
 func remove_item(item: Item, count: int):
@@ -28,8 +29,10 @@ func remove_item(item: Item, count: int):
 		negative_item_count.emit()
 	elif new_count == 0:
 		items.erase(item)
+		save_character()
 	else:
 		items[item] = new_count
+		save_character()
 
 
 func get_item_count(item: Item) -> int:
@@ -42,6 +45,7 @@ func get_money() -> Money:
 
 func set_money(new_money: Money):
 	money = Money.new(new_money.golden, new_money.silver, new_money.copper)
+	save_character()
 	
 	
 func get_char_name() -> String:
@@ -57,3 +61,31 @@ func get_total_weight() -> float:
 	for item in items.keys():
 		total += item.weight
 	return total
+
+func save_character():
+	var save = {}
+	if FileAccess.file_exists("res://database/save_test.json"):
+		var save_string = FileAccess.open("res://database/save_test.json",FileAccess.READ)
+		save = JSON.parse_string(save_string.get_as_text())
+	else:
+		save = {}
+	for char in save:
+		if char == char_name:
+			parse_items(save)
+			save[char]["money"] = parse_money()
+	var new_save_string = FileAccess.open("res://database/save_test.json",FileAccess.WRITE)
+	var json_string = JSON.stringify(save, " ", 4)
+	new_save_string.store_string(json_string)
+
+
+func parse_items(save):
+	var res_item = ""
+	var res_count = ""
+	for item in items:
+		res_item += str(item.id) + ", "
+		res_count += str(get_item_count(item)) + ", "
+	save[char_name]["items"] = res_item
+	save[char_name]["count"] = res_count
+		
+func parse_money():
+	return str(get_money().golden) + " " + str(get_money().silver) + " " + str(get_money().copper)
