0c2cec23944ab07b82a0dbd59f2b79b2cd30189d
samuraychik <vitvit0312@gmail.com>
2024-01-09 14:07:39 +0500

    Update CharacterCard.gd

--- interface/scenes/CharacterCard.gd
+++ interface/scenes/CharacterCard.gd
@@ -1,21 +1,21 @@
 extends Control
-var save_name
+
 signal character_deleted
 signal character_renamed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	save_name = "eblanchik"
+var char_name: String
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func set_char_name(new_name: String):
+	char_name = new_name
+	$PickButton.text = char_name
+
 
 func on_delete():
-	character_deleted.emit(save_name)
+	character_deleted.emit(char_name)
+	
 	
 func on_rename(new_name):
-	character_renamed.emit(save_name, new_name)
-	save_name = new_name
+	character_renamed.emit(char_name, new_name)
+	set_char_name(new_name)
 	
